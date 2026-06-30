9b8575dcf8d7e9eb1585a85b7bf11cc2ac6eb71a
bilodav <bilodavids@gmail.com>
2026-06-30 19:29:55 +0200

    Better styling and some images

--- client/src/pages/apologiaPage.jsx
+++ client/src/pages/apologiaPage.jsx
@@ -1,4 +1,6 @@
 import apologiaHome from "../assets/videos/apologiaHome.mp4";
+import apologiaHomeBanner1 from "../assets/apologiaImages/apologia1.jpeg";
+import apologiaHomeBanner2 from "../assets/apologiaImages/apologia2.jpeg";
 
 function ApologiaPage() {
   return (
@@ -8,9 +10,13 @@ function ApologiaPage() {
         and exhort, with complete patience and teaching.{" "}
       </h2>
       <p>2 Timothy 4:2</p>
-      <video autoPlay muted loop controls>
-        <source src={apologiaHome} type="video/mp4" />
-      </video>
+      <div className="apologia-landing-banner">
+        <img src={apologiaHomeBanner1} alt="" />
+        <video autoPlay muted loop controls>
+          <source src={apologiaHome} type="video/mp4" />
+        </video>
+        <img src={apologiaHomeBanner2} alt="" />
+      </div>
       <h2>
         but in your hearts honor Christ the Lord as holy, always being prepared
         to make a defense to anyone who asks you for a reason for the hope that
